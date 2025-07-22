acd888d6f2e1844a431dcb5455b0a32ad04967d7
saivamshi4121 <palamurivamshi2005@gmail.com>
2025-07-22 23:25:13 +0530

    first commit

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const connectDB = require('./config/db');
 dotenv.config();
 connectDB();
 
+
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
